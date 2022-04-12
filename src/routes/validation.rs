80ec63dd858cc809813f02a2ee1a9b4773d33604
Dione <dionemorales@outlook.com>
2022-04-12 20:31:56 +1000

    changed validate word endpoint response

--- src/routes/validation.rs
+++ src/routes/validation.rs
@@ -15,6 +15,6 @@ pub async fn validate_word(
     let word = request.0.word.to_lowercase();
     let date_index = 1;
     let word_to_guess = get_word_for_date(date_index, &db_connection).await.expect("failed to query word to guess");
-    println!("word to guess: {}", word_to_guess);
-    Ok(HttpResponse::Ok().body(format!("word to validate: {}", word)))
+
+    Ok(HttpResponse::Ok().body(format!("word to validate: {}\nword to guess: {}", word, word_to_guess)))
 }
